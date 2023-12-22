fd5b440a471dd1c2b47769cc6d8c69908adbcfeb
bbb169 <827088092@qq.com>
2023-12-22 19:50:30 +0800

    feat: add notes for user to add permission for osascript

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -7,6 +7,8 @@ function App(): JSX.Element {
       <Versions />
       <div style={{ textAlign: 'center' }}>
         请在系统设置的辅助功能中允许touch-panel-emulator控制您的电脑
+        同时还需要您允许osascript控制您的电脑，打开辅助功能系统设置后，步骤如下：
+        点击‘+’号，cmd+shift+G,输入/usr/bin/osascript,点击osascript即可添加
       </div>
       <MouseController />
     </div>
