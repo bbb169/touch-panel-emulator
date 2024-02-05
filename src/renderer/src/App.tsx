2fac86c0fd29ad3b84352fa4d353ae284bd54148
jingzouzou <827088092@qq.com>
2024-02-05 19:46:48 +0800

    feat: 修改界面UI

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,18 +1,11 @@
 import Versions from './components/Versions'
 import { DeviceInfoComp } from './components/deviceInfo/deviceInfo'
-import { WifiIpAddress } from './components/wifiIpAddress'
 
 function App(): JSX.Element {
   return (
     <div className="container">
       <Versions />
       <DeviceInfoComp />
-      <div style={{ textAlign: 'center' }}>
-        请在系统设置的辅助功能中允许touch-panel-emulator控制您的电脑
-        同时还需要您允许osascript控制您的电脑，打开辅助功能系统设置后，步骤如下：
-        点击‘+’号，cmd+shift+G,输入/usr/bin/osascript,点击osascript即可添加
-      </div>
-      <WifiIpAddress />
     </div>
   )
 }
